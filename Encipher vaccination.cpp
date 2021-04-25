631b865f3416b8b47436478705aa644c1ee735c1
Avinash7770 <83113519+Avinash7770@users.noreply.github.com>
2021-04-25 21:46:33 +0530

    Add files via upload

---
+++ Encipher vaccination.cpp
@@ -0,0 +1,232 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	system("cls")
+	cout<<"*********Vaccination Utsav*************"<<endl;
+	cout<<"Address-Durgakund Varanasi 221005"<<endl;
+	cout<<"               Press 1 to get information about covid19 vaccination         "<<endl;
+	cout<<"               Press 2 to know the criteria for covid19 vaccination         "<<endl;
+	cout<<"               Press 3 to know the registration procedure                   "<<endl;
+	cout<<"*Enter the age to access the information about vaccinated candidates!"<<endl;
+	int Age;
+	int index;
+
+	cin>>Age;
+	cin>>index;
+	if(index=1){
+		cout<<"What is vaccination:"<<endl;
+		cout<<"                Vaccination is a simple, safe, and effective way of protecting people against harmful "<<endl;
+		cout<<"                diseases, before they come into contact with them. It uses your body’s natural defenses"<<endl;
+		cout<<"                to build resistance to specific infections and makes your immune system stronger.      "<<endl;
+
+        cout<<"                Vaccines train your immune system to create antibodies, just as it does when it’s exposed"<<endl;
+        cout<<"                to a disease. However, because vaccines contain only killed or weakened forms of germs like"<<endl;
+        cout<<"                viruses or bacteria, they do not cause the disease or put you at risk of its complications."<<endl;
+
+
+        cout<<"                Most vaccines are given by an injection, but some are given orally (by mouth) or sprayed into the nose."<<endl;
+        cout<<endl;
+        cout<<endl;
+        cout<<endl;
+        cout<<endl;
+        cout<<endl;
+        cout<<endl;
+	}
+	else if(index=2){
+		cout<<endl;
+        cout<<endl;
+        cout<<endl;
+        cout<<endl;
+        cout<<endl;
+        cout<<endl;
+		cout<<"Who is eligible:"<<endl;
+		cout<<"                From March 1, India begins its next phase of coronavirus vaccination, to immunise senior citizens"<<endl;
+		cout<<"                and people with co-morbidities in the age bracket of 45-59 years."<<endl;
+
+        cout<<"                While walk-in vaccination will begin in many states on March 1, government officials expect a week"<<endl;
+        cout<<"                to pass before the system settles. People can start registering on the upgraded version of the app"<<endl;
+        cout<<"                CoWIN from March 1, although they may experience traffic and delay on the portal, as lakhs would "<<endl;
+        cout<<"                attempt to register themselves."<<endl;
+        cout<<endl;
+        cout<<endl;
+        cout<<endl;
+        cout<<endl;
+        cout<<endl;
+        cout<<endl;
+        cout<<endl;
+	}
+	else if(index =3){
+		cout<<endl;
+        cout<<endl;
+        cout<<endl;
+        cout<<endl;
+        cout<<endl;
+        cout<<endl;
+		cout<<"Registration procedure:"<<endl;
+		cout<<"How to register for Covid-19 vaccination on CoWIN?"<<endl;
+        cout<<"                Download CoWIN from app store or visit cowin.gov.in. One has to feed mobile number or Aadhar number"<<endl;
+        cout<<"                or any other government identification number to register. Once registered, vaccination centres in "<<endl;
+        cout<<"                the vicinity will be displayed. The Co-WIN 2.0 app is GPS-integrated, it will display nearby centres"<<endl;
+        cout<<"                based on a person’s mobile location. A person can choose a centre and a time slot as per convenience."<<endl;
+
+
+        cout<<"                Alternatively, one can walk into a centre without registration, but if the quota for the day is over,"<<endl;
+        cout<<"                the centre can ask them to return the next day. Vaccination centres have been directed to reserve 40 per"<<endl;
+        cout<<"                cent doses for pre-registered recipients and 60 per cent for walk-ins. This can change from centre to centre."<<endl;
+
+        cout<<"                Government hospitals will vaccinate for free, while in private hospitals, a recipient has to pay a maximum of"<<endl;
+        cout<<"                Rs 250 per dose. For two doses, that comes to Rs 500."<<endl;
+        cout<<endl;
+        cout<<endl;
+        cout<<endl;
+        cout<<endl;
+        cout<<endl;
+        cout<<endl;
+        cout<<endl;
+	}
+
+
+
+
+
+
+
+
+
+	if(45<=Age<50){
+		cout<<endl;
+        cout<<endl;
+        cout<<endl;
+        cout<<endl;
+        cout<<endl;
+        cout<<endl;
+		cout<<"Name-Suraj Yadav;           Sex-Male;        Dose-1;         Vaccine Name- Covishield;        Contact-6478924136"<<endl;
+		cout<<"Name-Mrityunjay Kumar;      Sex-Male;        Dose-1;         Vaccine Name-Covaxin;            Contact-9580828887"<<endl;
+		cout<<"Name-Harshit Rai;           Sex-Male;        Dose-1;         Vaccine Name- Covishield;        Contact-8299309439"<<endl;
+		
+		cout<<"Name-Elon Musk;             Sex-Male;        Dose-2;         Vaccine Name- Covaxin;           Contact-8052967963"<<endl;
+		cout<<"Name-Shivam Singh;          Sex-Male;        Dose-1;         Vaccine Name- Covishield;        Contact-5286389287"<<endl;
+		cout<<"Name-Rahul Kumar;           Sex-Male;        Dose-1;         Vaccine Name- Covaxin;           Contact-8459795412"<<endl;
+		cout<<"Name-Avinash Maurya;        Sex-Male;        Dose-1;         Vaccine Name- Covishield;        Contact-9567574655"<<endl;
+		cout<<"Name-AB De villiers;        Sex-Male;        Dose-1;         Vaccine Name-Covaxin;            Contact-2598756546"<<endl;
+		cout<<"Name-Yash Vikram Singh;     Sex-Male;        Dose-1;         Vaccine Name- Covishield;        Contact-9878575624"<<endl;
+		
+		cout<<"Name-Virat Kohli;           Sex-Male;        Dose-2;         Vaccine Name- Covaxin;           Contact-8456877854"<<endl;
+		cout<<"Name-Rishabh Pant;          Sex-Male;        Dose-1;         Vaccine Name- Covishield;        Contact-8489986545"<<endl;
+		cout<<"Name-Md Siraj;              Sex-Male;        Dose-1;         Vaccine Name- Covaxin;           Contact-5998745544"<<endl;
+		cout<<endl;
+		cout<<endl;
+		cout<<endl;
+		cout<<endl;
+		cout<<endl;
+		cout<<endl;
+		cout<<endl;
+		cout<<endl;
+
+
+	}
+	else if(50<=Age<60){
+		cout<<endl;
+        cout<<endl;
+        cout<<endl;
+        cout<<endl;
+        cout<<endl;
+        cout<<endl;
+		cout<<"Name-Vivek Kashyap;         Sex-Male;        Dose-1;         Vaccine Name- Covishield;        Contact-7789856428"<<endl;
+		cout<<"Name-Shubham Kumar;         Sex-Male;        Dose-1;         Vaccine Name-Covaxin             Contact-5486689935"<<endl;
+		cout<<"Name-Govind;                Sex-Male;        Dose-1;         Vaccine Name- Covishield;        Contact-7894554886"<<endl;
+		
+		cout<<"Name-Mukesh Ambani;         Sex-Male;        Dose-2;         Vaccine Name- Covaxin;           Contact-8985448898"<<endl;
+		cout<<"Name-Shakti Singh;          Sex-Male;        Dose-2;         Vaccine Name- Covishield;        Contact-6999999999"<<endl;
+		cout<<"Name-Shailesh Tiwari;       Sex-Male;        Dose-1;         Vaccine Name- Covaxin;           Contact-4779965489"<<endl;
+		cout<<"Name-Shubham Singh;         Sex-Male;        Dose-1;         Vaccine Name- Covishield;        Contact-9556599996"<<endl;
+		cout<<"Name-Ayush Bhardwaj;        Sex-Male;        Dose-2;         Vaccine Name-Covaxin;            Contact-5488511548"<<endl;
+		cout<<"Name-Ashutosh Tiwari;       Sex-Male;        Dose-1;         Vaccine Name- Covishield;        Contact-7899546599"<<endl;
+		
+		cout<<"Name-Santosh Yadav;         Sex-Male;        Dose-2;         Vaccine Name- Covaxin;           Contact-5959989889"<<endl;
+		cout<<"Name-Akash Kumar;           Sex-Male;        Dose-1;         Vaccine Name- Covishield;        Contact-4484655999"<<endl;
+		cout<<"Name-Krishnakant;           Sex-Male;        Dose-1;         Vaccine Name- Covaxin;           Contact-9898995699"<<endl;
+		cout<<endl;
+		cout<<endl;
+		cout<<endl;
+		cout<<endl;
+		cout<<endl;
+		cout<<endl;
+		cout<<endl;
+		cout<<endl;
+		cout<<endl;
+
+	
+
+
+	}
+	else if(Age>=60){
+		cout<<endl;
+        cout<<endl;
+        cout<<endl;
+        cout<<endl;
+        cout<<endl;
+        cout<<endl;
+		cout<<"Name-Abjhikalp Mishra;      Sex-Male;        Dose-1;         Vaccine Name- Covishield;        Contact-6475484136"<<endl;
+		cout<<"Name-Naval Kishore Shahi;   Sex-Male;        Dose-1;         Vaccine Name-Covaxin;            Contact-9580857787"<<endl;
+		cout<<"Name-Prshant Singh;         Sex-Male;        Dose-1;         Vaccine Name- Covishield;        Contact-8299358639"<<endl;
+		
+		cout<<"Name-Anik Pandey;           Sex-Male;        Dose-2;         Vaccine Name- Covaxin;           Contact-8052524963"<<endl;
+		cout<<"Name-Ds Rai;                Sex-Male;        Dose-1;         Vaccine Name- Covishield;        Contact-5286389257"<<endl;
+		cout<<"Name-OP Singh;              Sex-Male;        Dose-1;         Vaccine Name- Covaxin;           Contact-8452595412"<<endl;
+		cout<<"Name-Ak Jha;                Sex-Male;        Dose-1;         Vaccine Name- Covishield;        Contact-9567589655"<<endl;
+		cout<<"Name-DN Singh;              Sex-Male;        Dose-1;         Vaccine Name-Covaxin;            Contact-8569588546"<<endl;
+		cout<<"Name-KS Singh;              Sex-Male;        Dose-1;         Vaccine Name- Covishield;        Contact-9878250624"<<endl;
+		
+		cout<<"Name-Praveen Shukla;        Sex-Male;        Dose-2;         Vaccine Name- Covaxin;           Contact-8459878854"<<endl;
+		cout<<"Name-Ram Kumar Singh;       Sex-Male;        Dose-1;         Vaccine Name- Covishield;        Contact-8496374545"<<endl;
+		cout<<"Name-Aslam Sheikh;          Sex-Male;        Dose-1;         Vaccine Name- Covaxin;           Contact-5998525944"<<endl;
+
+		cout<<"Name-Vivek Laha;            Sex-Male;        Dose-1;         Vaccine Name- Covishield;        Contact-7789885828"<<endl;
+		cout<<"Name-Saurabh Singh;         Sex-Male;        Dose-1;         Vaccine Name-Covaxin             Contact-5486685535"<<endl;
+		cout<<"Name-Vipul Parmar;          Sex-Male;        Dose-1;         Vaccine Name- Covishield;        Contact-7894554336"<<endl;
+		
+		cout<<"Name-Ashok Dinda;           Sex-Male;        Dose-2;         Vaccine Name- Covaxin;           Contact-8562548898"<<endl;
+		cout<<"Name-Finn Allen;            Sex-Male;        Dose-2;         Vaccine Name- Covishield;        Contact-6896999999"<<endl;
+		cout<<"Name-Harish Tiwari;         Sex-Male;        Dose-1;         Vaccine Name- Covaxin;           Contact-4759665489"<<endl;
+		cout<<"Name-Anubhav Singh;         Sex-Male;        Dose-1;         Vaccine Name- Covishield;        Contact-9556598996"<<endl;
+		cout<<"Name-Rahul Vaidya;          Sex-Male;        Dose-2;         Vaccine Name-Covaxin;            Contact-5488515248"<<endl;
+		cout<<"Name-Ashwini Yadav;         Sex-Male;        Dose-1;         Vaccine Name- Covishield;        Contact-7899598899"<<endl;
+		
+		cout<<"Name-Dwiyam Gupta;          Sex-Male;        Dose-2;         Vaccine Name- Covaxin;           Contact-5959637889"<<endl;
+		cout<<"Name-Rohan Singh;           Sex-Male;        Dose-1;         Vaccine Name- Covishield;        Contact-4484741999"<<endl;
+		cout<<"Name-Sahil Khan;            Sex-Male;        Dose-1;         Vaccine Name- Covaxin;           Contact-9898896699"<<endl;
+		cout<<endl;
+		cout<<endl;
+		cout<<endl;
+		cout<<endl;
+		cout<<endl;
+		cout<<endl;
+		cout<<endl;
+		cout<<endl;
+		cout<<endl;
+		cout<<endl;
+
+
+	}
+	else{
+		cout<<endl;
+        cout<<endl;
+        cout<<endl;
+        cout<<endl;
+        cout<<endl;
+        cout<<endl;
+		cout<<"These age groups are yet to be vaccinated!"<<endl;
+		cout<<endl;
+		cout<<endl;
+		cout<<endl;
+		cout<<endl;
+		cout<<endl;
+		cout<<endl;
+		cout<<endl;
+	}
+	cout<<"470 People are vaccinated on our center."<<endl;
+	cout<<"1200 People are registered."<<endl;
+	return 0;
+}
